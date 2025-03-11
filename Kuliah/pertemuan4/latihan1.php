7630743a0e9381ce98710bbda270845d91c216a1
trayen9 <grahadestrian9@gmail.com>
2025-03-11 22:34:47 +0700

    commit

---
+++ Kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+// PENGULANGAN / LOOP / LOOPING
+// while
+// komponen while:
+// 1. nilai awal / inialisasi
+// 2. kondisi terminasi / kapan harus berhenti
+// 3. increment / decrement
+$i = 5;
+while ($i >= 1 ) {
+    echo "Hello World! $i kali <br>";
+    $i --;
+}
+
+echo "<hr>";
+
+// for
+// for(nilai_awal; kondisi_terminasi; increment/dec){
+// aksi
+//}
+
+for($i=1; $i<=10; $i++){
+    echo "Hello World! $i x <br>";
+}
+
